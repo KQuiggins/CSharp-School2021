cc008d3b52d7e7f1d660049004caff97eb30cecb
Kenneth Darrick Quiggins <kashfamilyvalues@gmail.com>
2021-11-08 19:15:23 -0500

    Add files via upload

---
+++ EnterInvoices/EnterInvoices/EnterInvoices/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnterInvoices
+{
+    public partial class Form1 : Form
+    {
+        const string DELIM = ",";
+        const string FILENAME =
+            @"C:\CSharp\Chapter14\Invoices.txt";
+        int num;
+        string name;
+        double amount;
+        static FileStream outFile = new
+            FileStream(FILENAME, FileMode.Create, FileAccess.Write);
+        StreamWriter writer = new StreamWriter(outFile);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(invoiceBox.Text);
+            name = nameBox.Text;
+            amount = Convert.ToDouble(amountBox.Text);
+            writer.WriteLine(num + DELIM + name + DELIM + amount);
+            invoiceBox.Clear();
+            nameBox.Clear();
+            amountBox.Clear();
+        }
+    }
+}
