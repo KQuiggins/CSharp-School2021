d67a090df2e7d455d5c84d8bfe3841792f4b905f
Kenneth Darrick Quiggins <kashfamilyvalues@gmail.com>
2021-11-08 19:05:52 -0500

    Add files via upload

---
+++ DailySpecial/DailySpecial/DailySpecial/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace DailySpecial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dayBox = new System.Windows.Forms.TextBox();
+            this.specialButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(161, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a day number to see our special";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(231, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "For example, Sunday = 1";
+            // 
+            // dayBox
+            // 
+            this.dayBox.Location = new System.Drawing.Point(343, 179);
+            this.dayBox.Name = "dayBox";
+            this.dayBox.Size = new System.Drawing.Size(70, 26);
+            this.dayBox.TabIndex = 2;
+            // 
+            // specialButton
+            // 
+            this.specialButton.Location = new System.Drawing.Point(311, 251);
+            this.specialButton.Name = "specialButton";
+            this.specialButton.Size = new System.Drawing.Size(133, 35);
+            this.specialButton.TabIndex = 3;
+            this.specialButton.Text = "Get Special";
+            this.specialButton.UseVisualStyleBackColor = true;
+            this.specialButton.Click += new System.EventHandler(this.SpecialButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(351, 346);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 20);
+            this.outputLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 483);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.specialButton);
+            this.Controls.Add(this.dayBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dayBox;
+        private System.Windows.Forms.Button specialButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
